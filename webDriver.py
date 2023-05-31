11221161eab4752ee883754943dab5d77f1a7284
Thomas Wood <80734198+Thomas-Wood-Eng@users.noreply.github.com>
2023-05-31 11:54:18 -0700

    Create webDriver.py

---
+++ webDriver.py
@@ -0,0 +1,64 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+from enum import Enum
+
+driver = webdriver.Chrome()
+
+itemList = {"Bread", "Dairy", "Meat", "Fruit", "Vegetables"}
+
+
+class Stores(Enum):
+    no_frills = "https://www.nofrills.ca/"
+    save_on_foods = "https://www.saveonfoods.com/sm/pickup/rsid/1982/"
+    superstore = "https://www.realcanadiansuperstore.ca/"
+    urban_fare = "https://www.urbanfare.com/sm/pickup/rsid/7615/"
+    safeway = "https://voila.ca/"
+    # iga = "https://www.iga.net/en"
+
+# iga is just the worst and doesn't work when the rest do
+
+
+
+def main():
+    #search_all_stores("Bread")
+    search_all_stores(itemList)
+    time.sleep(60)
+    driver.quit()
+
+def search_multiple_products(store_url, product_keyword_list):
+    driver.switch_to.new_window('tab')
+    driver.get(store_url)
+
+    for item in product_keyword_list:
+        search_bar = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "input")))
+        search_bar.click()
+        search_bar.clear()
+        search_bar.send_keys(item)
+        search_bar.send_keys(Keys.RETURN)
+        time.sleep(3)
+
+def search_product(store_url, product_keywords):
+    driver.switch_to.new_window('tab')
+    driver.get(store_url)
+    search_bar = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "input")))
+
+    search_bar.click()
+    search_bar.clear()
+    search_bar.send_keys(product_keywords)
+    search_bar.send_keys(Keys.RETURN)
+
+
+def search_all_stores(product_keywords):
+    for store in Stores:
+        #search_product(store.value, product_keywords)
+        search_multiple_products(store.value, product_keywords)
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
