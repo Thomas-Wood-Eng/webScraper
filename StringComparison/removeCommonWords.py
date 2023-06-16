a6d0cd24aca0eb82e57c30f2a166fdf752b6c6a0
Thenuja Liyanage <Liyanage.Thenuja@hotmail.com>
2023-06-15 20:51:27 -0400

    Added function to remove common words

---
+++ StringComparison/removeCommonWords.py
@@ -0,0 +1,23 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+def remove_stopwords(text):
+    # Download stopwords and punkt
+    nltk.download('stopwords')
+    nltk.download('punkt')
+
+    # Get the list of stopwords
+    stop_words = set(stopwords.words('english'))
+
+    # Tokenize the text
+    tokens = word_tokenize(text)
+
+    # Remove stopwords
+    filtered_tokens = [word for word in tokens if word.casefold() not in stop_words]
+
+    # Reconstruct the string without stopwords
+    filtered_text = ' '.join(filtered_tokens)
+
+    return filtered_text
+
