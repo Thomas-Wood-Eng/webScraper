283c4e5f9b8bafce129b95872bdeedca6f95b587
Thenuja Liyanage <Liyanage.Thenuja@hotmail.com>
2023-07-25 21:22:34 -0400

    Added methods to insert and get pks for stores, brands, and measurement_unit

--- Scraper/dbInsert.py
+++ Scraper/dbInsert.py
@@ -28,6 +28,7 @@ import json
 class DBINSERT:
     def __init__(self, productGroupList:list[dict]) -> None:
         self.productList = productGroupList
+        self.insertedPoducts = set()
         load_dotenv()
 
     def connect(self):
@@ -58,7 +59,49 @@ class DBINSERT:
 
     # insert store if not found and return the pk, if found return the pk without inserting     
     def insert_store(self, merchant_id:int, province_id:int, merchant_storeid:str, location_name:str):
-        pass
+        self.cur.execute(''' SELECT * FROM stores WHERE merchant_id = (%s) AND merchant_storeid = (%s)
+                            LIMIT 1 ;
+                            ''', (merchant_id, merchant_storeid))
+        store = self.cur.fetchone()
+
+        if(store != None):
+            return store[0] # return pk from store table entry
+        
+        self.cur.execute(''' INSERT INTO sotres (merchant_id, province_id, merchant_storeid, location_name)
+                             VALUES (%s, %s, %s, %s);''', (merchant_id, province_id, merchant_storeid, location_name))
+        
+        self.cur.execute(''' SELECT * FROM stores WHERE merchant_id = (%s) AND merchant_storeid = (%s)
+                            LIMIT 1 ;
+                            ''', (merchant_id, merchant_storeid))
+        return self.cur.fetchone()[0]
+        
+    def insert_brand(self, brand_name:str):
+        self.cur.execute(''' SELECT * FROM brands WHERE brand_name = (%s)
+                            LIMIT 1 ;
+                            ''', (brand_name,))
+        brand = self.cur.fetchone()
+
+        if(brand != None):
+            return brand[0]
+        
+        self.cur.execute(''' INSERT INTO brands (brand_name)
+                             VALUES (%s)''', (brand_name,))
+        self.cur.execute(''' SELECT * FROM brands WHERE brand_name = (%s)
+                            LIMIT 1 ;
+                            ''', (brand_name,))
+        return self.cur.fetchone()[0]
+
+
+    def get_meassurementUnit_pk(self, unit:str):
+        self.cur.execute(''' SELECT * FROM meassurement_units WHERE lower(unit_name) = lower(%s)
+                            LIMIT 1 ;
+                            ''', (unit,))
+        unit_entry = self.cur.fetchone()
+
+        if(unit_entry != None):
+            return unit_entry[0]
+        else:
+            return unit_entry # this returns Nonw -> throw an error from the calling function if None is returned for debudding.
 
     # inserts/updates a product in/to the database and returns pk after insertion or if found
     def dbInsertProduct(self, product:dict):
@@ -76,13 +119,19 @@ class DBINSERT:
         
         # check if product in db
         self.cur.execute(''' SELECT * FROM products WHERE lower(product_name) = lower(%s) 
-                             AND merchant_product_id = (%s)
+                             AND merchant_productid = (%s)
                              LIMIT 1''', (name, merchant_product_id))
         product_selectResult = self.cur.fetchone()
-
+        
         if(product_selectResult != None):
-            # if product already exisits, update the total_price column value
+            # check is product was already inserted/updated in this session
+            if(product_selectResult[0] in self.insertedPoducts):
+                return
+            else:
+                self.insert_store.add(product_selectResult[0])
 
+            # if product already exisits, update the total_price column value
+            self.cur.execute() # CODE TO UPDATE THE total_price column value of the entry
             return
         
      
@@ -90,8 +139,17 @@ class DBINSERT:
         merchant_pk = self.insert_merchant(merchant_name)
     
         # insert store
-        pass
-           
+        store_pk = self.insert_store(1, 2, "1077", 'GRANDVIEW - VANCOUVER')
+
+        # insert brand
+        brand_pk = self.insert_brand(brand)
+
+        # get pk of meassurement unit
+        meassurementUnit_pk = self.get_meassurementUnit_pk(size_unit)
+        if(meassurementUnit_pk == None):
+            raise ValueError(f'''unit {size_unit} is not available in the db. Please insert this unit into the 'meassurement_units' table or correct the unit in the parser.\n\nproduct: {product}
+                                ''')
+        # insert staqtement for prodcut 
 
     def dbInsertGroups(self) -> None:
         
